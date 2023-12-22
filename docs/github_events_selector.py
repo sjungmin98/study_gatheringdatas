e88c74fa777458269fb96ad0d6953da3951cf74f
sjungmin98 <shc980@gmail.com>
2023-12-22 17:42:57 +0900

    11

--- docs/github_events_selector.py
+++ docs/github_events_selector.py
@@ -8,14 +8,25 @@ browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
 # Chrome WebDriver의 capabilities 속성 사용
 capabilities = browser.capabilities
 # - 주소 https://www.w3schools.com/ 입력
-browser.get("https://www.w3schools.com/")
+browser.get("https://github.com/login")
 # - 가능 여부에 대한 OK 받음
 pass
 # - html 파일 받음(and 확인)
 html = browser.page_source
-print(html)
+# print(html)
 # - 정보 획득
+from selenium.webdriver.common.by import By
+
+element_login_field = browser.find_element(by=By.CSS_SELECTOR, value="#login_field")
+element_login_field.send_keys("shc980@gmail.com")
+
+element_password_field = browser.find_element(by=By.CSS_SELECTOR, value="#password")
+element_password_field.send_keys("Jmhj0216^^")
 pass
-browser.save.screenshot("./format.png")
+element_login_button = browser.find_element(by=By.CSS_SELECTOR, value="div > input.btn.btn-primary.btn-block.js-sign-in-button")
+element_login_button.click()
+pass
+# browser.save.screenshot("./format.png")
+
 # 브라우저 종료
 browser.quit()
--- docs/quests/jobplanet_login_selector.py
+++ docs/quests/jobplanet_login_selector.py
@@ -8,14 +8,25 @@ browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
 # Chrome WebDriver의 capabilities 속성 사용
 capabilities = browser.capabilities
 # - 주소 https://www.w3schools.com/ 입력
-browser.get("https://www.w3schools.com/")
+browser.get("https://www.jobplanet.co.kr/users/sign_in?_nav=gb")
 # - 가능 여부에 대한 OK 받음
 pass
 # - html 파일 받음(and 확인)
 html = browser.page_source
-print(html)
+# print(html)
 # - 정보 획득
+from selenium.webdriver.common.by import By
+
+element_login_field = browser.find_element(by=By.CSS_SELECTOR, value="#user_email")
+element_login_field.send_keys("shc980@gmail.com")
+
+element_password_field = browser.find_element(by=By.CSS_SELECTOR, value="#user_password")
+element_password_field.send_keys("SECRET")
 pass
-browser.save.screenshot("./format.png")
+element_login_button = browser.find_element(by=By.CSS_SELECTOR, value="#signInSignInCon > div.signInsignIn_wrap > div > section.section_email.er_r > fieldset > button")
+element_login_button.click()
+pass
+# browser.save.screenshot("./format.png")
+
 # 브라우저 종료
 browser.quit()
